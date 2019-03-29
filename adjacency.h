8464888ee556ff2628939af5413b092f34420bfe
CarsonTaylor <itsonlycarson@gmail.com>
2019-03-29 01:29:43 -0500

    comments

--- adjacency.h
+++ adjacency.h
@@ -1,6 +1,8 @@
+//variables for index calculations
 int dim;
 int sz;
 
+//sets neighbouring indexes in ascending order for squares in the middle
 void setMiddleNeighbours(int** list, int index){
   int k = 0;
   list[index][k] = index - dim - 1;
@@ -22,7 +24,9 @@ void setMiddleNeighbours(int** list, int index){
   return;
 }
 
+//sets neighbouring indexes in ascending order for squares in the corners
 void setCornerNeighbours(int** list, int index){
+  //top left corner
   if(index == 0){
     int k = 0;
     list[index][k] = index + 1;
@@ -31,6 +35,7 @@ void setCornerNeighbours(int** list, int index){
     k++;
     list[index][k] = index + dim + 1;
   }
+  //top right corner
   else if(index == dim-1){
     int k = 0;
     list[index][k] = index - 1;
@@ -39,6 +44,7 @@ void setCornerNeighbours(int** list, int index){
     k++;
     list[index][k] = index + dim;
   }
+  //bottom left corner
   else if(index == sz - dim){
     int k = 0;
     list[index][k] = index - dim;
@@ -47,6 +53,7 @@ void setCornerNeighbours(int** list, int index){
     k++;
     list[index][k] = index + 1;
   }
+  //bottom right corner
   else if(index == sz - 1){
     int k = 0;
     list[index][k] = index - dim - 1;
@@ -58,6 +65,7 @@ void setCornerNeighbours(int** list, int index){
   return;
 }
 
+//sets neighbouring indexes in ascending order for squares on the edges
 void setEdgeNeighbours(int** list, int index){
   //left edge
   if(index%dim == 0){
@@ -114,11 +122,14 @@ void setEdgeNeighbours(int** list, int index){
   return;
 }
 
+//returns an adjacency list representing a square boggle
+//board of dimension n
 int** adjacencyList(int n){
   int** list;
   dim = n;
   sz = n*n;
 
+  //allocate and initialize list
   list = (int **)malloc(sz * sizeof(int *));
   for(int i=0; i<sz; i++)
        list[i] = (int *)malloc(9 * sizeof(int));
@@ -126,14 +137,16 @@ int** adjacencyList(int n){
     for(int j = 0; j < 9; j++)
       list[i][j] = -1;
 
+  //allocate array for types of squares
   int **type = (int **)malloc(dim * sizeof(int *));
   for(int i=0; i<dim; i++)
        type[i] = (int *)malloc(dim * sizeof(int));
+  //initialize type array
+  for(int i = 0; i < dim; i++)
+    for(int j = 0; j < dim; j++)
+      type[i][j] = 0;
 
-   for(int i = 0; i < dim; i++)
-     for(int j = 0; j < dim; j++)
-       type[i][j] = 0;
-
+  //set type array values for corners and edges
   for(int i = 0; i < dim; i++){
     for(int j = 0; j < dim; j++){
       //corners
@@ -146,6 +159,7 @@ int** adjacencyList(int n){
     }
   }
 
+  //assign appropiate neighbours for each index based on type
   int count = 0;
   for(int i = 0; i < dim; i++){
     for(int j = 0; j < dim; j++){
